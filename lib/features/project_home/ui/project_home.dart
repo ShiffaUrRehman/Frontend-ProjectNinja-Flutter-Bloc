f8cc53dbe17b78abc670716c6e14ee407c9d3f4c
Shiffa Ur Rehman <22100167@lums.edu.pk>
2023-10-10 16:26:28 +0500

    Home Project is scrollable

--- lib/features/project_home/ui/project_home.dart
+++ lib/features/project_home/ui/project_home.dart
@@ -54,171 +54,174 @@ class _ProjectHomeState extends State<ProjectHome> {
           switch (state.runtimeType) {
             case ProjectHomeLoadedState:
               final projectLoaded = state as ProjectHomeLoadedState;
-              return Container(
-                color: Colors.grey.shade300,
-                padding: const EdgeInsets.all(10),
-                margin: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        const Text('Name: ',
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.w400)),
-                        Flexible(
-                          child: Text(
-                            projectLoaded.project.name,
-                            style: const TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.w500),
+              return SingleChildScrollView(
+                child: Container(
+                  color: Colors.grey.shade300,
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          const Text('Name: ',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w400)),
+                          Flexible(
+                            child: Text(
+                              projectLoaded.project.name,
+                              style: const TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w500),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Description: ',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w400),
-                        ),
-                        Flexible(
-                          child: Text(
-                            projectLoaded.project.description,
-                            style: const TextStyle(
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Description: ',
+                            style: TextStyle(
                                 fontSize: 16, fontWeight: FontWeight.w400),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        const Text(
-                          'Status: ',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400),
-                        ),
-                        Text(
-                          projectLoaded.project.status,
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        (LoginRepo.user.role == 'Admin' ||
-                                LoginRepo.user.role == 'Project Manager')
-                            ? PopupMenuButton(
-                                initialValue: projectLoaded.project.status,
-                                child: Container(
-                                  padding: const EdgeInsets.all(10),
-                                  decoration: const BoxDecoration(
-                                      color: Colors.blue,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(5))),
-                                  child: const Text(
-                                    'Change',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ),
-                                onSelected: (String item) {
-                                  projectHomeBloc.add(ProjectChangeStatusEvent(
-                                      status: item,
-                                      projectId: widget.project.id));
-                                },
-                                itemBuilder: (BuildContext context) =>
-                                    <PopupMenuEntry<String>>[
-                                  const PopupMenuItem<String>(
-                                    value: "Onboarding",
-                                    child: Text('Onboarding'),
-                                  ),
-                                  const PopupMenuItem<String>(
-                                    value: "In Progress",
-                                    child: Text('In Progress'),
-                                  ),
-                                  const PopupMenuItem<String>(
-                                    value: "Complete",
-                                    child: Text('Complete'),
-                                  ),
-                                ],
-                              )
-                            : const SizedBox()
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        const Text('Project Manager: ',
+                          Flexible(
+                            child: Text(
+                              projectLoaded.project.description,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            'Status: ',
                             style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w400)),
-                        Flexible(
-                          child: Text(
-                            projectLoaded.project.projectManager.fullname,
+                                fontSize: 20, fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                            projectLoaded.project.status,
                             style: const TextStyle(
                                 fontSize: 20, fontWeight: FontWeight.w400),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        const Text('Members ',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w400)),
-                        const SizedBox(
-                          width: 50,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Members(
-                                      projectId: projectLoaded.project.id,
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          (LoginRepo.user.role == 'Admin' ||
+                                  LoginRepo.user.role == 'Project Manager')
+                              ? PopupMenuButton(
+                                  initialValue: projectLoaded.project.status,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: const BoxDecoration(
+                                        color: Colors.blue,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5))),
+                                    child: const Text(
+                                      'Change',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500),
                                     ),
-                                  ));
-                            },
-                            child: const Text('View'))
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        const Text('Tasks ',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w400)),
-                        const SizedBox(
-                          width: 80,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => TasksHome(
-                                      projectId: projectLoaded.project.id,
+                                  ),
+                                  onSelected: (String item) {
+                                    projectHomeBloc.add(
+                                        ProjectChangeStatusEvent(
+                                            status: item,
+                                            projectId: widget.project.id));
+                                  },
+                                  itemBuilder: (BuildContext context) =>
+                                      <PopupMenuEntry<String>>[
+                                    const PopupMenuItem<String>(
+                                      value: "Onboarding",
+                                      child: Text('Onboarding'),
                                     ),
-                                  ));
-                            },
-                            child: const Text('View'))
-                      ],
-                    ),
-                  ],
+                                    const PopupMenuItem<String>(
+                                      value: "In Progress",
+                                      child: Text('In Progress'),
+                                    ),
+                                    const PopupMenuItem<String>(
+                                      value: "Complete",
+                                      child: Text('Complete'),
+                                    ),
+                                  ],
+                                )
+                              : const SizedBox()
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Text('Project Manager: ',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w400)),
+                          Flexible(
+                            child: Text(
+                              projectLoaded.project.projectManager.fullname,
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Text('Members ',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w400)),
+                          const SizedBox(
+                            width: 50,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Members(
+                                        projectId: projectLoaded.project.id,
+                                      ),
+                                    ));
+                              },
+                              child: const Text('View'))
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Text('Tasks ',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w400)),
+                          const SizedBox(
+                            width: 80,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => TasksHome(
+                                        projectId: projectLoaded.project.id,
+                                      ),
+                                    ));
+                              },
+                              child: const Text('View'))
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               );
             case ProjectHomeLoadingState:
